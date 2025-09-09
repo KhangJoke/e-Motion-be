ef06c5d3f64542d2b6178da3f4628bd64679048c
KhangJoke <khangngoc3082005@gmail.com>
2025-09-09 15:00:47 +0700

    initial commit

---
+++ src/main/java/com/swp391/e_Motion_be/EMotionBeApplication.java
@@ -0,0 +1,13 @@
+package com.swp391.e_Motion_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMotionBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMotionBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swp391/e_Motion_be/EMotionBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp391.e_Motion_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMotionBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
