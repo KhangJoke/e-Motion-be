e138d0396796fc75addc5dd7804990412cc807e8
KhangJoke <khangngoc3082005@gmail.com>
2025-10-01 14:41:44 +0700

    [SWP391-87] Create Cancel Reservation Flow and Send Reservation Code to Email

---
+++ src/main/java/com/swp391/e_Motion_be/dto/requests/payment/RefundRequest.java
@@ -0,0 +1,4 @@
+package com.swp391.e_Motion_be.dto.requests.payment;
+
+public class RefundRequest {
+}
---
+++ src/main/java/com/swp391/e_Motion_be/dto/responses/RefundResponse.java
@@ -0,0 +1,4 @@
+package com.swp391.e_Motion_be.dto.responses;
+
+public class RefundResponse {
+}
