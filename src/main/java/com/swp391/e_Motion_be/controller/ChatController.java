096547f791bec749b3e75fa59165ec092ba0447f
KhangJoke <khangngoc3082005@gmail.com>
2025-10-21 15:26:54 +0700

    Implement rent advise chatbox with Gemini 2.5 Flash model

--- src/main/java/com/swp391/e_Motion_be/controller/ChatController.java
+++ src/main/java/com/swp391/e_Motion_be/controller/ChatController.java
@@ -1,7 +1,9 @@
 package com.swp391.e_Motion_be.controller;
 
 import com.swp391.e_Motion_be.dto.requests.chat.ChatRequest;
+import com.swp391.e_Motion_be.dto.responses.ApiResponse;
 import com.swp391.e_Motion_be.service.ChatService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +18,10 @@ public class ChatController {
     private final ChatService chatService;
 
     @PostMapping("/message")
-    public String chatMessage(@RequestBody ChatRequest request){
-        return chatService.generation(request);
+    public ApiResponse<String> chatMessage(@RequestBody ChatRequest request, HttpSession session){
+        ApiResponse<String> response = new ApiResponse<>();
+        response.setMessage("Chat response generated successfully");
+        response.setData(chatService.generation(request, session.getId()));
+        return response;
     }
 }
--- src/main/java/com/swp391/e_Motion_be/dto/requests/chat/ChatRequest.java
+++ src/main/java/com/swp391/e_Motion_be/dto/requests/chat/ChatRequest.java
@@ -4,5 +4,5 @@ import lombok.Data;
 
 @Data
 public class ChatRequest {
-    private String question;
+    private String message;
 }
