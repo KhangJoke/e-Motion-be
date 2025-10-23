438e1073d0e5bad0308a03b95a3a429070585f10
myapah76 <140422521+myapah76@users.noreply.github.com>
2025-10-23 15:16:50 +0700

    Add search by contains email in user

--- src/main/java/com/swp391/e_Motion_be/repository/UserRepository.java
+++ src/main/java/com/swp391/e_Motion_be/repository/UserRepository.java
@@ -5,6 +5,7 @@ import com.swp391.e_Motion_be.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByPhone(String phone);
     long countByRole(Role role);
+    List<User> findByEmailContains(String email);
 }
