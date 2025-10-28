a86468c6ab2ecb7a4b25c03d4a7a04970bc81dbd
KhangJoke <khangngoc3082005@gmail.com>
2025-10-28 10:25:24 +0700

    Update handler exception for unauthorized and accessdenied exception

--- src/main/java/com/swp391/e_Motion_be/exception/GlobalExceptionHandler.java
+++ src/main/java/com/swp391/e_Motion_be/exception/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ import com.swp391.e_Motion_be.enums.ErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -27,6 +29,32 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    }
+
+    //AccessDenied Exception
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiResponse<Object>> handleAccessDeniedException(Exception ex) {
+        ApiResponse<Object> response = new ApiResponse<>();
+        response.setStatus(403);
+        response.setMessage("Forbidden: " + ex.getMessage());
+        response.setData(null);
+
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(response);
+    }
+
+    //Unauthorized Exception
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiResponse<Object>> handleAuthenticationException(Exception ex) {
+        ApiResponse<Object> response = new ApiResponse<>();
+        response.setStatus(401);
+        response.setMessage("Unauthorized: " + ex.getMessage());
+        response.setData(null);
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
                 .body(response);
     }
 
