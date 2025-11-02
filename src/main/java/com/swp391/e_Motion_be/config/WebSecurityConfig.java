3bd6006cdb36ae4372089e9c1bb3e77014246f99
KhangJoke <khangngoc3082005@gmail.com>
2025-11-02 19:37:26 +0700

    Update authorization for verify otp

--- src/main/java/com/swp391/e_Motion_be/config/WebSecurityConfig.java
+++ src/main/java/com/swp391/e_Motion_be/config/WebSecurityConfig.java
@@ -62,7 +62,7 @@ public class WebSecurityConfig {
                 .csrf(csrf -> csrf.disable()) // disable CSRF nếu xài JWT
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Cấu hình CORS
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login", "/api/auth/register", "/api/auth/refresh").permitAll()
+                        .requestMatchers("/api/auth/login", "/api/auth/register", "/api/auth/refresh", "/api/auth/verify", "/api/auth/resend", "/api/auth/forgotPassword/**").permitAll()
                         .requestMatchers("/api/payment/vnpay-return").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/vehicles/**", "/api/stations/**", "/api/ratings/**").permitAll()
